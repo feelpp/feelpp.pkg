8dce4d0027bb0f5bfaac40cc838a1a4a476e2c9e
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2019-11-04 06:34:54 +0100

    update debian packaging
     . testcases are now in share/feelpp/data
     . provide path to deb packages to upload_bintray
     . cleanup

--- feelpp/upload_bintray.sh
+++ feelpp/upload_bintray.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
 
 version=$1
-repo=${2:-ubuntu}
-distribution=${3:-eoan}
-components=${4:-main}
-arch=${5:-amd64,i386}
-for i in *.deb *.changes; do
+rep=${2:-.}
+repo=${3:-ubuntu}
+distribution=${4:-eoan}
+components=${5:-main}
+arch=${6:-amd64,i386}
+for i in $rep/*.deb $rep/*.changes; do
     
     curl -X PUT -T $i -uprudhomm:${BINTRAY_API_KEY} "https://api.bintray.com/content/feelpp/$repo/feelpp/$version/pool/main/f/feelpp/$i;deb_distribution=$distribution;deb_component=$components;deb_architecture=$arch;publish=1"
 
