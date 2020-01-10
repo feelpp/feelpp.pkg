c940e42e4a69883d9df5b682a174164901cf63f3
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2020-01-10 06:08:56 +0100

    forgot to modify path with component/channel

--- feelpp/upload_bintray.sh
+++ feelpp/upload_bintray.sh
@@ -9,6 +9,6 @@ arch=${6:-amd64,i386}
 for i in $rep/*.deb $rep/*.changes; do
     
     filename=$(basename $i)
-    curl -X PUT -T $i -uprudhomm:${BINTRAY_API_KEY} "https://api.bintray.com/content/feelpp/$repo/feelpp/$version/$distribution/pool/main/f/feelpp/$filename;deb_distribution=$distribution;deb_component=$components;deb_architecture=$arch;publish=1;override=1"
+    curl -X PUT -T $i -uprudhomm:${BINTRAY_API_KEY} "https://api.bintray.com/content/feelpp/$repo/feelpp/$version/$distribution/pool/$components/f/feelpp/$filename;deb_distribution=$distribution;deb_component=$components;deb_architecture=$arch;publish=1;override=1"
 
 done
