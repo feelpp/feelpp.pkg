05313c340310c714c30932d7438b58c45fff1bc8
Christophe Prud'homme <christophe.prudhomme@cemosis.fr>
2021-05-24 22:10:45 +0000

    fix cmd

--- feelpp-mor/upload_bintray.sh
+++ feelpp-mor/upload_bintray.sh
@@ -1,15 +1,1 @@
-#! /bin/sh
-
-version=$1
-rep=${2:-.}
-repo=${3:-ubuntu}
-distribution=${4:-focal}
-channel=${5:-latest}
-component=${6:-feelpp}
-arch=${7:-amd64,i386}
-for i in $rep/*.deb $rep/*.changes; do
-    
-    filename=$(basename $i)
-    curl -X PUT -T $i -uprudhomm:${BINTRAY_API_KEY} "https://api.bintray.com/content/feelpp/$repo/feelpp/$version/$distribution/pool/$channel/f/$component/$filename;deb_distribution=$distribution;deb_component=$channel;deb_architecture=$arch;publish=1;override=1"
-
-done
+../feelpp/upload_bintray.sh
