2a6484fb11247eb181d76be156dd3fdf6ff658c7
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2022-11-06 12:52:45 +0100

    clean python debs and fix mor

--- feelpp-python/focal/debian/gen_python_tb_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for i in core coefficientformpdes heat electric fluid solid hdg fsi heatfluid thermoelectric; do
-cat > python3-feelpp-toolboxes-$i.install << EOF
-usr/lib/python3*/*-packages/feelpp/toolboxes/$i/*.py
-usr/lib/python3*/*-packages/feelpp/toolboxes/$i/*.so
-EOF
-done
