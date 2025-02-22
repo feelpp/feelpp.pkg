77253c9fcfa8ad2ea9e7745bc469440eb40611db
Christophe Prud'homme <christophe.prudhomme@cemosis.fr>
2025-02-22 07:37:54 +0100

    use GPG now

--- aptly.sh
+++ aptly.sh
@@ -4,7 +4,7 @@ dist=${1:-focal}
 if test "$dist" = "focal" -o "$dist" = "kinetic" -o "$dist" = "jammy" -o "$dist" = "noble"; then
     flavor=ubuntu
 fi
-if test "$dist" = "buster" -o "$dist" = "bullseye" -o "$dist" = "bookworm" -o "$dist" = "testing" -o "$dist" = "sid"; then
+if test "$dist" = "buster" -o "$dist" = "bullseye" -o "$dist" = "bookworm" -o "$dist" = "trixie" -o "$dist" = "testing" -o "$dist" = "sid"; then
     flavor=debian
 fi
 echo "dist: $dist  -- flavor: $flavor" 
@@ -12,12 +12,10 @@ echo "dist: $dist  -- flavor: $flavor"
 aptly repo create -distribution=$dist -component=stable feelpp-$dist-stable
 aptly repo create -distribution=$dist -component=latest feelpp-$dist-latest
 
-if test -z "$BUILDKITE_PASSPHRASE"; then
+if test -z "$GPG_PASSPHRASE"; then
     echo "passphrase not defined"
     return
 else
     echo "passphrase defined..."
-    echo $BUILDKITE_PASSPHRASE > pp
-    aptly publish repo -passphrase-file=pp -architectures="amd64" -component="stable,latest" feelpp-$dist-stable feelpp-$dist-latest s3:apt.feelpp.org:$flavor/$dist
-    rm pp
+    aptly publish repo -passphrase="${GPG_PASSPHRASE}" -gpg-key=${GPG_KEY}  -architectures="amd64" -component="stable,latest" feelpp-$dist-stable feelpp-$dist-latest s3:apt.feelpp.org:$flavor/$dist
 fi
